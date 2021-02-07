dbf9bbb22d85b4434932c9525cea29626a79791c
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2021-02-07 22:41:47 +0100

    Add files via upload

---
+++ cvekŠpraljaJakovBiljka/cvekŠpraljaJakovBiljka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovBiljka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bijelogoricno Hrast = new Bijelogoricno(true);
+            Crnogoricno Bor = new Crnogoricno(false);
+        }
+    }
+}
